91e561171a49d2ef41f68dc5c3cd50d99a07db96
George Hodulik <ghodulik95@gmail.com>
2014-10-01 16:57:56 -0400

    Added functions

--- Interpreter.scm
+++ Interpreter.scm
@@ -1,6 +1,6 @@
 ;George Hodulik Project 2 EECS 345
-(load "loopSimpleParser.scm")
-;My interpreter does not implement the "extra challenge" features.
+(load "functionParser.scm")
+;My interpreter DOES implement the extra challenge of call by reference, but does NOT do statements with side effects.
 ;My general code convention:
 ;Parameter code indicates code that could have come from the parser, ie, one or more statements contained in a list
 ;Parameter statement indicates one statement, not contained in an extra list
@@ -10,22 +10,24 @@
 ;An expression is an arithmetic operation, ie it is a value, a var, or aritmetic operations of values and/or vars.
 ;Ex. expressions: x, y, (+ x y), (* x (+ 4 y))
 
+
 ;takes code and environment and returns the environment or the environment of the next statememnt
 (define Mstate
   (lambda (code environment return next break continue)
-   ; (begin (display environment)
     (cond
       ((null? code) (next environment))
       (else (Mstatement (nextstatement code) environment return (lambda (env) (Mstate (remainingstatements code) env return
                                                                                       next break continue))
                                                                   break continue)))))
-           ;)     
+            
 ;takes code an an environment and acts upod the nextstatement
 (define Mstatement
   (lambda (statement environment return next break continue)
     (cond
       ;if this is reached, perform the next action on the resulting environment
       ((null? statement) (next environment))
+      ;If it is a function call
+      ((funcall? statement) (begin (call-function (cadr statement) (cddr statement) environment) (next environment)));(next (Mstatefunc statement environment)))
       ;if this is a block, call Mstate on it, with a new layer to the environment, and be sure to cdr off that layer when next, break, or continue is called
       ((block? statement) (Mstate (remainingstatements statement) (addLayer environment) return (lambda (v) (next (cdr v))) (lambda (v) (break (cdr v))) (lambda (v) (continue (cdr v))) ))
       ;calls if statement if nextstatement is if: note, remaining statements is a parameter, since the if statement could have a return, or it could continue
@@ -43,6 +45,14 @@
       ;if this is a while, call whileloop
       ((while? statement) (whileloop statement environment return next break continue)))))
 
+(define Mstate-build
+  (lambda (code environment)
+    (cond
+      ((null? code) environment)
+      ((declaration? (nextstatement code)) (Mstate-build (cdr code) (declarevar (nextstatement code) environment)))
+      ((function-decl? (nextstatement code)) (Mstate-build (cdr code) (Mstatefunc (nextstatement code) environment)))
+      (else (error "Invalid operations outside of main function")))))
+
 ;gets the nextstatement from code. returns a statement
 (define nextstatement
   (lambda (code)
@@ -57,6 +67,15 @@
 (define addLayer
   (lambda (environment)
     (cons '() environment)))
+
+(define funcall?
+  (lambda (statement)
+    (eq? (car statement) 'funcall)))
+
+;checks if a statement is a function
+(define function-decl?
+  (lambda (statement)
+    (eq? (car statement) 'function)))
 
 ;checks if a statement is a block
 (define block?
@@ -99,6 +118,52 @@
 (define continue?
   (lambda (statement)
     (eq? (car statement) 'continue)))
+;The below functions build what needs to be added to the environment for a function : (fname fclosure)
+(define functionname
+  (lambda (statement)
+    (cadr statement)))
+(define functionbody
+  (lambda (statement)
+    (cadddr statement)))
+(define functionformalparams
+  (lambda (statement)
+    (caddr statement)))
+
+;conses the closure of the function to the environment when the function is written
+(define Mstatefunc
+  (lambda (statement environment)
+    (addLayer (cons (fclosure statement) environment))))
+
+;makes the closure of a function
+(define fclosure
+  (lambda (statement)
+    (list (list (functionname statement) (box (list (functionformalparams statement) (functionbody statement) fenvironment))))))
+
+;builds the environment based off the environment at call time: note the environment is cdred at the end so as to not include
+;the environment from when the function is called.  Note that when there is an &, the get-box function is called rather 
+;than eval-expression
+(define fenvironment
+  (lambda (fparams cparams environment)
+    (cond
+      ((and (null? fparams) (null? cparams)) (cdr environment))
+      ((or (null? fparams) (null? cparams)) (error "Parameter mistmatch"))
+      ((eq? (car fparams) '&) (cons (list (list (cadr fparams) (get-box (car cparams) environment))) (fenvironment (cddr fparams) (cdr cparams) environment)))
+      (else (cons (list(list (car fparams) (box (eval-expression (car cparams) environment pprefix)))) (fenvironment (cdr fparams) (cdr cparams) environment))))))
+
+;get-box and lookupBox are identical to lookup and lookupLayer, except they do not unbox the var's value
+(define get-box
+  (lambda (varname environment)
+    (cond
+      ((null? environment) (error "variable not declared"))
+      ((not (null? (lookupBox varname (car environment)))) (lookupBox varname (car environment)))
+      (else (get-box varname (cdr environment))))))
+
+(define lookupBox
+  (lambda (var environment)
+    (cond
+      ((null? environment) '())
+      ((eq? (caar environment) var) (cadar environment))
+      (else (lookupBox var (cdr environment))))))
 
 ;gets the condition of a while
 (define whilecondition
@@ -150,7 +215,7 @@
       ((eq? assigntype 'declaration) (error "redefining of variable"))
       ((eq? (lookupLayer varname (car environment)) '()) (cons (car environment) (assign varname value (cdr environment) original-environment assigntype)))
       (else (cons (assignLayer varname value (car environment) original-environment assigntype) (cdr environment))))))
-    
+   
 ;assignLayer assigns a value for a specific layer
 (define assignLayer
   (lambda (varname value environment original-environment assigntype)
@@ -164,7 +229,7 @@
       ;if the var is reached but this is a declaration, the variable is being redeclared
       ((and (eq? (caar environment) varname) (eq? assigntype 'declaration) (error "redefining of variable")))
       ;if the var is reached otherwise, update its value
-      ((eq? (caar environment) varname) (cons (list varname (box (eval-expression value original-environment pprefix))) (cdr environment)))
+      ((eq? (caar environment) varname) (begin (set-box! (cadar environment) (eval-expression value original-environment pprefix)) environment))
       ;otherwise recurse
       (else (cons (car environment) (assignLayer varname value (cdr environment) original-environment assigntype))))))
 
@@ -189,7 +254,7 @@
       ((null? environment) '())
       ((eq? (caar environment) var) (unbox (cadar environment)))
       (else (lookupLayer var (cdr environment))))))
-
+ 
 ;lookup determines which layer of the environment (or the first appearance) contains var, and calls lookupList to get its value
 (define lookup
   (lambda (var environment)
@@ -268,6 +333,7 @@
          ;if the expression is an initialized var, return its value
          ((and (not (list? expression)) (not (number? expression))) (lookup expression environment))
          ;the next few lines perform arithmetic operations
+         ((funcall? expression) (call-function (cadr expression) (cddr expression) environment))
          ((eq? '+ (operator expression)) (+ (eval-expression (left-operand expression) environment form) (eval-expression (right-operand expression) environment form)))
          ;THIS LINE BELOW MUST BE CHANGED IF PARSER IS NO LONGER PREFIX to allow negatives support ( like -(* 5 9))
          ((and (eq? '- (operator expression)) (null? (cddr expression))) (- 0 (eval-expression (cadr expression) environment form)))
@@ -327,14 +393,43 @@
     (list car cadr caddr)))
 
 (define newEnvironment
-  '(()))
+  '(()) )
 
 ;interprets code by calling Mstate
 (define interpret
   (lambda (filename)
+    ;(legitimize 
+     (call-main (Mstate-build (parser filename) newEnvironment)))) ;return (lambda (v)  v) (lambda (v) v) (lambda (v) v))))))
+     ;(call/cc (lambda (return)
+       ;(call-main (Mstate (parser filename) newEnvironment return (lambda (v)  v) (lambda (v) v) (lambda (v) v))))))
+
+;abstraction functions to get parts of the function closure
+(define getformalparams
+  (lambda (closure)
+    (car closure)))
+
+(define getfunctionbody
+  (lambda (closure)
+    (cadr closure)))
+
+(define getenvbuildfunction
+  (lambda (closure)
+    (caddr closure)))
+
+;call the main function - same as call function except parameter list is always empty and the name of the function is known
+(define call-main
+  (lambda (environment)
     (legitimize 
      (call/cc (lambda (return)
-       (Mstate (parser filename) newEnvironment return (lambda (v)  v) (lambda (v) v) (lambda (v) v)))))))
+       (letrec ((f (lookup 'main environment)))
+        (Mstate (getfunctionbody f) ((getenvbuildfunction f) (getformalparams f) '() environment) return (lambda (v) v) (lambda (v) v) (lambda (v) v))))))))
+
+;calls a function
+(define call-function
+  (lambda (fname params environment)
+     (call/cc (lambda (return2)
+       (letrec ((f (lookup fname environment)))
+        (Mstate (cadr f) (addLayer ((caddr f) (car f) params environment)) return2 (lambda (v) v) (lambda (v) v) (lambda (v) v)))))))
 
 ;legitimizes return value: ie, if it is not a number or boolean, that means a return statement was not reached
 (define legitimize
@@ -347,6 +442,10 @@
       (else (error "No return reached")))))
             
 
+;(null? (car (lookup 'main (Mstate-build 
+ ;(parser "test25.txt") 
+ ;newEnvironment))))
+
 ;Test function: will test a list of testfiles and return the list of their return values
 (define doTests
   (lambda (tests)
@@ -358,7 +457,7 @@
 ;same on computers other than mine.
 ;tests for part 2
 ;(doTests 
- ;'("test25.txt" "test26.txt" 
+ ;'("test25.txt")); "test26.txt" 
  ;"test27.txt" "test28.txt" "test29.txt" "test30.txt" "test31.txt" ))
  ;              '("test34.txt")); "test33.txt" "test34.txt" 
  ;"test35.txt" 
